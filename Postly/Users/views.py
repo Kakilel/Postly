f3973b095a49a12f774e85ecbf85a6dba5f79dff
ckakilel@gmail.com <kakilelbarwett@gmail.com>
2025-09-16 01:37:17 +0300

    fixed api intergration and responses

--- Postly/Users/views.py
+++ Postly/Users/views.py
@@ -84,12 +84,15 @@ def add_user(request):
         users = CustomUser.objects.all()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
-    if request.method == 'POST':
+    
+    elif request.method == 'POST':
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
         return Response(serializer.data,status= status.HTTP_201_CREATED)
     return Response(serializer.error,status=status.HTTP_400_BAD_REQUEST)
+
+
 @api_view(['GET','PUT','DELETE'])
 def user_list(request,pk):
     try:
@@ -101,13 +104,13 @@ def user_list(request,pk):
         serializer = UserSerializer(user)
         return Response(serializer.data)
     
-    if request.method == 'PUT':
+    elif request.method == 'PUT':
         serializer = UserSerializer(user,data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST) 
-    if request.method == 'DELETE':
+    elif request.method == 'DELETE':
         user.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)       
 

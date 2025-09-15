4e8560bafbe47b5a7ec6608a19b1bdb8a92aa48b
ckakilel@gmail.com <kakilelbarwett@gmail.com>
2025-09-16 01:05:22 +0300

    fixed info and added more serializers

--- Postly/App/serializer.py
+++ Postly/App/serializer.py
@@ -1,22 +1,50 @@
 from rest_framework import serializers
-from .models import *
+from django.contrib.auth import get_user_model
+from .models import Post, Category, Comment, Like
 
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = '__all__'
+User = get_user_model()
 
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = '__all__'
 
-class CategorySerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Category
-        fields = '__all__'
+        model = User
+        fields = ["id", "username", "email"]
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ["id", "author", "body", "created_at"]
+
 
 class LikeSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
     class Meta:
         model = Like
-        fields = '__all__'
+        fields = ["id", "user", "timestamp"]
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+    category = serializers.StringRelatedField()
+    comments = CommentSerializer(many=True, read_only=True)
+    likes = LikeSerializer(many=True, source="like_set", read_only=True)
+
+    class Meta:
+        model = Post
+        fields = [
+            "id", "title", "author", "category", "content", "image",
+            "created_at", "updated_at", "published_date", "featured",
+            "total_likes", "comments", "likes"
+        ]
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    posts = PostSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Category
+        fields = ["id", "name", "slug", "posts"]
